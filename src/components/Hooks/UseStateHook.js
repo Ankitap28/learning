ef788652a22492e2b834659caa058566daaf1d71
farmeazy <farmeazy@gmail.com>
2022-02-21 23:35:23 +0530

    third

--- src/components/Hooks/UseStateHook.js
+++ src/components/Hooks/UseStateHook.js
@@ -4,8 +4,8 @@ function UseStateHook() {
      const [count, setCount] = useState(0)
   return (
     <div>
-      {/* <button onClick={() => setCount(count + 1)}>Count : {count}</button> */}
-      <button onClick={() => setCount(count + 1)}>Count : {count}</button>
+      {/* <button onClick={() => setCount(count + 1)}>Count : {count}</button> in this previous state is not stored */ }
+      <button onClick={() => setCount(prevCount=> prevCount + 1)}>Count : {count}</button>
     </div>
   );
 }
